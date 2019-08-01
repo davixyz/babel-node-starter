2ad25c11dc3b054ca6b4a18bec398b3b754d16ee
Carlos Castro <cdcastrofelix@gmail.com>
2019-07-31 20:21:17 -0700

    chore: upgrade dependencies

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,9 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express.default)();
+var app = (0, _express["default"])();
 var port = 3000;
 app.get('/', function (req, res) {
   return res.send('Hello World!');
